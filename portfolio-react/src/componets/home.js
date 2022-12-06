829b6813bf7104244784ea78a4b023a2c634600e
Akash Chanara <akashchanara@gmail.com>
2022-12-05 23:36:58 -0800

    css home page

--- portfolio-react/src/componets/home.js
+++ portfolio-react/src/componets/home.js
@@ -1,15 +1,15 @@
 import React from "react";
 import img from "../img/img.jpeg";
+
 import "../styles/Home.css";
 function Home() {
   return (
     <div className="home">
       <div className="about">
         <img src={img} alt="me" />
-        <h2>Hi, My name is Akash Chanara</h2>
+        <h1>Hi, I am Akash Chanara</h1>
         <p>A full stack developer and like to learn new technologies </p>
       </div>
-      <div className="skill"></div>
     </div>
   );
 }
