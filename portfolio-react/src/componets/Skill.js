fd1ebdd0b4f5bd855fdef4af18c64d40d0e2e3c2
Akash Chanara <akashchanara@gmail.com>
2022-12-06 20:30:37 -0800

    skill pages

--- portfolio-react/src/componets/Skill.js
+++ portfolio-react/src/componets/Skill.js
@@ -1,7 +1,66 @@
 import React from "react";
+import "../styles/Skill.css";
+import img3 from "../img/img3.webp";
+import img4 from "../img/img4.png";
+import img5 from "../img/img5.png";
+import img6 from "../img/img6.png";
+import img7 from "../img/img7.png";
+import img8 from "../img/img8.png";
+import img9 from "../img/img9.webp";
+import img10 from "../img/img10.png";
+import resume from "../img/Akash_Chanara_Resume .pdf";
 
 function Skill() {
-  return <div>Skill</div>;
+  return (
+    <div className="skill">
+      <div className="resume">
+        <a href={resume} download="Akash_Chanara_Resume .pdf">
+          Click here download Resume
+        </a>
+      </div>
+      <div className="list">
+        <div>Python</div>
+        <div>Java </div>
+        <div>Visual Studio</div>
+        <div> Android Studio</div>
+        <div> MySQL</div>
+        <div>HTML/CSS </div>
+        <div>Hadoop</div>
+        <div>CUDA</div>
+        <div>Moment.js</div>
+        <div>Bootstrap</div>
+        <div>Node.js</div>
+        <div>MS Visio</div>
+        <div>Ubuntu</div>
+        <div>Oracle</div>
+        <div>JavaScript</div>
+        <div> Fetch/Ajax</div>
+        <div>Unit Testing/Jest </div>
+        <div>Server side</div>
+        <div>API</div>
+        <div>ES6</div>
+        <div>Microsoft Office</div>
+        <div>MongodB</div>
+        <div>REST API </div>
+        <div>Express</div>
+        <div>Heroku</div>
+        <div>Git/GitHub</div>
+        <div>GrapghQl</div>
+        <div>Third party API</div>
+        <div>Relational Database jQuery</div>
+      </div>
+      <div className="imgall">
+        <img src={img3} alt="me" />
+        <img src={img4} alt="me" />
+        <img src={img5} alt="me" />
+        <img src={img6} alt="me" />
+        <img src={img7} alt="me" />
+        <img src={img8} alt="me" />
+        <img src={img9} alt="me" />
+        <img src={img10} alt="me" />
+      </div>
+    </div>
+  );
 }
 
 export default Skill;
--- portfolio-react/src/componets/contact.js
+++ portfolio-react/src/componets/contact.js
@@ -1,7 +1,31 @@
 import React from "react";
 import "../styles/Contact.css";
+// import React, { useRef } from "react";
+// import emailjs from "@emailjs/browser";
 
 function Contact() {
+  // const form = useRef();
+
+  // const sendEmail = (e) => {
+  //   e.preventDefault();
+
+  //   // emailjs
+  //   //   .sendForm(
+  //   //     "YOUR_SERVICE_ID",
+  //   //     "YOUR_TEMPLATE_ID",
+  //   //     form.current,
+  //   //     "YOUR_PUBLIC_KEY"
+  //   //   )
+  //     .then(
+  //       (result) => {
+  //         console.log(result.text);
+  //       },
+  //       (error) => {
+  //         console.log(error.text);
+  //       }
+  //     );
+  // };
+
   return (
     <div className="contact">
       <div className="head">
