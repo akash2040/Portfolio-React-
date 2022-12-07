570b2b150da29ec0b94c29e12ef6bfe4d5f9bb7a
Akash Chanara <akashchanara@gmail.com>
2022-12-06 23:04:20 -0800

    button

--- portfolio-react/src/componets/contact.js
+++ portfolio-react/src/componets/contact.js
@@ -1,6 +1,7 @@
 import React from "react";
+
 import "../styles/Contact.css";
-// import React, { useRef } from "react";
+// import { useRef } from "react";
 // import emailjs from "@emailjs/browser";
 
 function Contact() {
@@ -9,19 +10,15 @@ function Contact() {
   // const sendEmail = (e) => {
   //   e.preventDefault();
 
-  //   // emailjs
-  //   //   .sendForm(
-  //   //     "YOUR_SERVICE_ID",
-  //   //     "YOUR_TEMPLATE_ID",
-  //   //     form.current,
-  //   //     "YOUR_PUBLIC_KEY"
-  //   //   )
+  //   emailjs
+  //     .sendForm("gmail", "template_YeJhZkgb", form.current, "your-token")
   //     .then(
-  //       (result) => {
-  //         console.log(result.text);
+  //       () => {
+  //         alert("Message successfully sent!");
+  //         window.location.reload(false);
   //       },
-  //       (error) => {
-  //         console.log(error.text);
+  //       () => {
+  //         alert("Failed to send the message, please try again");
   //       }
   //     );
   // };
@@ -37,32 +34,21 @@ function Contact() {
       </div>
       <div className="form">
         <form>
-          <ul>
-            <li className="form-input">
-              <input placeholder="Name" type="text" name="name" required />
-            </li>
-            <li className="form-input">
-              <input placeholder="Email" type="email" name="email" required />
-            </li>
-            <li className="form-input">
-              <input
-                placeholder="Subject"
-                type="text"
-                name="subject"
-                required
-              />
-            </li>
-            <div className="form-input">
-              <textarea
-                placeholder="Message"
-                name="message"
-                required
-              ></textarea>
-            </div>
-            <div>
-              <input type="submit" className="flat-button" value="SEND" />
-            </div>
-          </ul>
+          <div className="form-input">
+            <input placeholder="Name" type="text" name="name" required />
+          </div>
+          <div className="form-input">
+            <input placeholder="Email" type="email" name="email" required />
+          </div>
+          <div className="form-input">
+            <input placeholder="Subject" type="text" name="subject" required />
+          </div>
+          <div className="form-input">
+            <textarea placeholder="Message" name="message" required></textarea>
+          </div>
+          <div>
+            <input type="submit" className="button" value="SEND" />
+          </div>
         </form>
       </div>
     </div>
